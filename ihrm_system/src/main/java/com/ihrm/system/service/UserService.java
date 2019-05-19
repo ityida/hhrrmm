3612dcaa1c627a817476200a126a64ab18ee633b
yida <zhangyfei126@yeah.net>
2019-05-19 10:57:32 +0800

    权限 和 jjwt

--- ihrm_system/src/main/java/com/ihrm/system/service/UserService.java
+++ ihrm_system/src/main/java/com/ihrm/system/service/UserService.java
@@ -1,7 +1,9 @@
 package com.ihrm.system.service;
 
 import com.ihrm.common.utils.IdWorker;
+import com.ihrm.domain.system.Role;
 import com.ihrm.domain.system.User;
+import com.ihrm.system.dao.RoleDao;
 import com.ihrm.system.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,15 +16,16 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private RoleDao roleDao;
 
     @Autowired
     private IdWorker idWorker;
@@ -110,5 +113,30 @@ public class UserService {
      */
     public void deleteById(String id) {
         userDao.deleteById(id);
+    }
+
+    /**
+     * 分配角色
+     */
+    public void assignRoles(String userId,List<String> roleIds) {
+        //1.根据id查询用户
+        User user = userDao.findById(userId).get();
+        //2.设置用户的角色集合
+        Set<Role> roles = new HashSet<>(10);
+        for (String roleId : roleIds) {
+            Role role = roleDao.findById(roleId).get();
+            roles.add(role);
+        }
+        //设置用户和角色集合的关系
+        user.setRoles(roles);
+        //3.更新用户
+        userDao.save(user);
+    }
+
+    /**
+     * 根据mobile查询用户
+     */
+    public User findByMobile(String mobile) {
+        return userDao.findByMobile(mobile);
     }
 }
